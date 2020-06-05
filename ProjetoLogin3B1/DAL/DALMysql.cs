d3be434f877fd20a58836a81d353ec47bbc4fefd
otavianocotemig <otaviano@cotemig.com.br>
2020-06-05 11:14:49 -0300

    Inserção da Classe DAL

---
+++ ProjetoLogin3B1/DAL/DALMysql.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLogin3B1.DAL
+{
+    public class DALMysql
+    {
+       private string string_conexao = "Persist Security info=false ; server=localhost; database=dbmvc;user=root;pwd=;";
+       private MySqlConnection conexao;
+    
+       public void Conectar()
+        {
+            try
+            {
+                conexao = new MySqlConnection(string_conexao);
+                conexao.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Problemas na conexão com o Banco de Dados. " + e.Message);
+            }
+        }
+
+        public void executarComando(string sql)
+        {
+            try
+            {
+                Conectar();
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Não foi possível executar a instrução no Banco de Dados. " + e.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+
+
+    }
+}
